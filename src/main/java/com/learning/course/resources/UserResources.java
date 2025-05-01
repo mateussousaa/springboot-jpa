0385cda7fa96007adb58cebff7010a8dc4648606
mateussousaa <mateussousa20@outlook.com>
2025-05-01 19:05:04 -0300

    feat: creating the service layer to user

--- src/main/java/com/learning/course/resources/UserResources.java
+++ src/main/java/com/learning/course/resources/UserResources.java
@@ -1,18 +1,30 @@
 package com.learning.course.resources;
 
 import com.learning.course.entities.User;
+import com.learning.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResources {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User("user", "user@mail.com", "5588999998888", "password");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
