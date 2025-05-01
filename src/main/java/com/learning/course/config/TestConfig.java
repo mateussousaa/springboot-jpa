eee373e9e4bdec00bb3c8de8a3fce8771c7f97bb
mateussousaa <mateussousa20@outlook.com>
2025-05-01 16:29:37 -0300

    test: creating a configuration to seed my db using the test profile

--- src/main/java/com/learning/course/config/TestConfig.java
+++ src/main/java/com/learning/course/config/TestConfig.java
@@ -1,15 +1,26 @@
 package com.learning.course.config;
 
+import com.learning.course.entities.User;
 import com.learning.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Arrays;
+
 @Configuration
 @Profile("test")
-
-public class TestConfig {
+public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User("Maria Brown", "maria@email.com", "88999999999", "genericPassword");
+        User u2 = new User("Alex Green", "alex@email.com", "88999999999", "genericPassword");
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
 }
